55e8af60e5e45c1626694a1600d0dabbf01de81d
Miklos Bertalan <miklos.bertalan@risingstack.com>
2018-04-04 17:20:05 +0200

    add code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import autoBlur from './autoBlur';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+autoBlur();
